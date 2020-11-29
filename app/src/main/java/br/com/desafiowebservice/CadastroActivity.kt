ddfd9927366b23f899fa126822780601292cc667
Rodrigo Freitas <fwtrodrigo@gmail.com>
2020-11-29 00:43:47 -0300

    Implementacao tela de cadastro

--- app/src/main/java/br/com/desafiowebservice/CadastroActivity.kt
+++ app/src/main/java/br/com/desafiowebservice/CadastroActivity.kt
@@ -1,11 +1,18 @@
 package br.com.desafiowebservice
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_cadastro.*
 
 class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
+
+        cadastroBtnSave.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
--- app/src/main/java/br/com/desafiowebservice/SplashActivity.kt
+++ app/src/main/java/br/com/desafiowebservice/SplashActivity.kt
@@ -13,6 +13,7 @@ class SplashActivity : AppCompatActivity() {
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, LoginActivity::class.java))
+            finish()
         }, 2000)
     }
 }
