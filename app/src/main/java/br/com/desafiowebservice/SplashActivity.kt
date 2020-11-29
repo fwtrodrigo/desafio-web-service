c62761e28be3d7a1af4ffc3a5e745784b79bc331
Rodrigo Freitas <fwtrodrigo@gmail.com>
2020-11-28 22:41:14 -0300

    Alteracao da cor do statusbar

--- app/src/main/java/br/com/desafiowebservice/SplashActivity.kt
+++ app/src/main/java/br/com/desafiowebservice/SplashActivity.kt
@@ -1,5 +1,6 @@
 package br.com.desafiowebservice
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -11,7 +12,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
-
+            startActivity(Intent(this, MainActivity::class.java))
         }, 2000)
     }
 }
