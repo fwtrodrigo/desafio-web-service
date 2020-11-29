c726cef0ec550229185267a8a28ca3b09279fb95
Rodrigo Freitas <fwtrodrigo@gmail.com>
2020-11-29 00:13:39 -0300

    Implementacao tela de login

---
+++ app/src/main/java/br/com/desafiowebservice/LoginActivity.kt
@@ -0,0 +1,24 @@
+package br.com.desafiowebservice
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        loginBtnLogin.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+
+        loginTvCreateAccount.setOnClickListener {
+            startActivity(Intent(this, CadastroActivity::class.java))
+            finish()
+        }
+    }
+}
