5da1df3bb7819fe98842dbf187042a0aa445a131
Rodrigo Freitas <fwtrodrigo@gmail.com>
2020-11-28 22:09:09 -0300

    adicao splash screen

---
+++ app/src/main/java/br/com/desafiowebservice/SplashActivity.kt
@@ -0,0 +1,17 @@
+package br.com.desafiowebservice
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+        }, 2000)
+    }
+}
