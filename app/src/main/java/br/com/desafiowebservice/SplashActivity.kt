81f4f69b5bf07859eb7dd7b09bb48c0e3ba58f35
Rodrigo Freitas <fwtrodrigo@gmail.com>
2020-11-30 15:58:33 -0300

    alterado a splash screen para rodar com coroutines

--- app/src/main/java/br/com/desafiowebservice/SplashActivity.kt
+++ app/src/main/java/br/com/desafiowebservice/SplashActivity.kt
@@ -2,18 +2,25 @@ package br.com.desafiowebservice
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
+
+    private val scope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, LoginActivity::class.java))
+        scope.launch {
+            delay(3000)
+            startActivity(Intent(application, LoginActivity::class.java))
             finish()
-        }, 2000)
+        }
+
     }
 }
